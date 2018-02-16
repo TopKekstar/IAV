66d347954c6b93fff6b36554e344093c52f09a42
Usuario Local <Usuario Local>
2018-02-16 10:42:54 +0100

    tsuuu

---
+++ IAV_Template/Assets/Scripts/Map.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+    Vector2[] direcciones = { new Vector2(0, 1), new Vector2(1, 0), new Vector2(0, -1), new Vector2(-1, 0) };
+
+    public Canvas canvas;
+ 
+        
+    // Use this for initialization
+    void Start() {
+     
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void setMap()
+    {
+
+    }
+  
+    public void mueveTile()
+    {
+
+    }
+}
---
+++ IAV_Template/Assets/Scripts/Tile.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+  
+        private int _x, _y;
+        private uint _tileID;
+        Tile(int x, int y, uint tileID)
+        {
+            _x = x;
+            _y = y;
+            _tileID = tileID;
+        }
+        public Tile getTile()
+        {
+            return this;
+        }
+        public void setX(int x)
+        {
+            _x = x;
+        }
+        public void setY(int y)
+        {
+            _y = y;
+
+        }
+
+    
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
