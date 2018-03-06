aff19b74875aaf4d9848daec6e7486273193a1d5
Usuario Local <Usuario Local>
2018-03-06 18:45:17 +0100

    mierda de practica loko

---
+++ IAV_2/Assets/Scripts/Mapa.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mapa : MonoBehaviour {
+	public GameObject prefabTile;
+	int anchoCasilla;
+	int altoCasilla;
+	Tile[,] mapaTiles;
+	// Use this for initialization
+	void Start () {
+		mapaTiles = new Tile[10,10];
+		anchoCasilla = 32;
+		altoCasilla = 32;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				GameObject auxT = Instantiate (prefabTile, gameObject.transform);
+				mapaTiles [i, j] = auxT.GetComponent<Tile> ();
+				mapaTiles [i, j].ini (0, 1);
+				mapaTiles [i, j].setPos (anchoCasilla * j, altoCasilla * i);
+				Debug.Log ("kek");
+				int k = 0;
+				k++;
+			}
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ IAV_2/Assets/Scripts/Tile.cs
@@ -0,0 +1,49 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+	
+	int nTile;
+	int coste;
+	public int posX;
+	public int posY;
+	bool movida;
+	// Use this for initialization
+	void Start () {
+		movida = false;
+		
+	}
+	public void ini(int nTile_,int coste_ ){
+		nTile = nTile_;
+		coste = coste_;
+		switch (nTile) {
+		case 0:
+			GetComponent<UnityEngine.UI.Image> ().color = Color.green;
+			break;
+		case 1:
+			GetComponent<UnityEngine.UI.Image> ().color = Color.blue;
+			break;
+		case 2:
+			GetComponent<UnityEngine.UI.Image> ().color = Color.gray;
+			break;
+		default:
+			break;
+		}
+		
+
+		int k = 0;
+		k++;
+	}
+	public void setPos(int pX,int pY){
+		posX = pX;
+		posY = pY;
+		gameObject.transform.localPosition.Set ((float)pX,(float) pY, 0);
+	}
+	// Update is called once per frame
+	void Update () {
+		if(movida)
+		
+	}
+}
