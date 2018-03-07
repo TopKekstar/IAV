73d62593226f6ee92db7dc07f595c4b21038540f
franlbc97 <franlbc97@gmail.com>
2018-03-07 13:39:02 +0100

    eheheheh

--- IAV_2/Assets/Scripts/GameManager.cs
+++ IAV_2/Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
     public static GameManager instance;
     public GameObject unidadActual;
+    public Mapa mapa;
 
     // Use this for initialization
     void Start () {
