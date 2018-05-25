51e08c21f0ac7a5a9a74031b211a4d8563fd61af
Usuario Local <Usuario Local>
2018-05-25 11:01:04 +0200

    su

---
+++ IAV_5/Assets/MouseDetect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseDetect : MonoBehaviour {
+
+	public GameObject npc; 
+
+	void OnMouseDown() {
+
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+			npc.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
